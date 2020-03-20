f39e5d7597e76f18bac24ce185b23ba9bce5ba77
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-03-19 19:52:40 -0700

    got schema working

--- server/src/graphql.rs
+++ server/src/graphql.rs
@@ -1,56 +1,47 @@
-use juniper::FieldResult;
-use juniper::RootNode;
-
-use juniper::{GraphQLObject};
+use juniper::{FieldResult,GraphQLObject,RootNode};
 
 #[derive(GraphQLObject)]
-#[graphql(description = "A humanoid creature in the Star Wars universe")]
 struct FindReplaceCommand {
     find: String,
     replace: String,
     command: String,
 }
+
 pub struct Context(pub i32);
+
 impl juniper::Context for Context {}
 
-pub struct QueryRoot;
+pub struct Query;
 
 #[juniper::object(
 // Here we specify the context type for this object.
 Context = Context,
 )]
-impl QueryRoot {
-    fn human(id: String) -> FieldResult<FindReplaceCommand> {
-        // Ok(Human {
-        //     id: "1234".to_owned(),
-        //     name: "Luke".to_owned(),
-        //     appears_in: vec![Episode::NewHope],
-        //     home_planet: "Mars".to_owned(),
-        // })
-        unimplemented!()
+impl Query {
+    #[graphql(description = "A tuple for the user's form submission")]
+    fn get_find_replace_command(id: String) -> FieldResult<Option<FindReplaceCommand>> {
+        Ok(Option::from(FindReplaceCommand {
+            find: "1234".to_owned(),
+            replace: "Luke".to_owned(),
+            command: "Mars".to_owned(),
+        }))
     }
 }
 
-pub struct MutationRoot;
+pub struct Mutation;
 
 #[juniper::object(
-// Here we specify the context type for this object.
 Context = Context,
 )]
-impl MutationRoot {
-    fn create_human() -> FieldResult<FindReplaceCommand> {
-        // Ok(Human {
-        //     id: "1234".to_owned(),
-        //     name: new_human.name,
-        //     appears_in: new_human.appears_in,
-        //     home_planet: new_human.home_planet,
-        // })
-        unimplemented!()
+impl Mutation {
+    #[graphql(description = "Returns a url for accessing the tupel")]
+    fn create_command(find: String, replace: String) -> FieldResult<String> {
+        Ok(String::from("Foo"))
     }
 }
 
-pub type Schema = RootNode<'static, QueryRoot, MutationRoot>;
+pub type Schema = RootNode<'static, Query, Mutation>;
 
 pub fn create_schema() -> Schema {
-    Schema::new(QueryRoot {}, MutationRoot {})
+    Schema::new(Query {}, Mutation {})
 }
