3ab34f5ddfb558bfddf19d58a0327a5976a91b8b
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-03-23 10:47:46 -0700

    clean up package.json

--- client/apollo.config.js
+++ client/apollo.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     client: {
         service: {
-            name: 'faxtail',
+            name: 'replace.sh',
             localSchemaFile: 'schema.graphql'
         }
     }
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,10 @@ import {ApolloProvider} from "@apollo/react-hooks";
 import ApolloClient from "apollo-client";
 import {InMemoryCache} from "apollo-cache-inmemory";
 import {PageContainer} from "./PageContainer";
-import {createUploadLink} from "apollo-upload-client";
-import {GRAPHQL_ENDPOINT} from "./config";
 import ReactGA from 'react-ga';
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
-    link: createUploadLink({uri: GRAPHQL_ENDPOINT})
 });
 
 export default function App() {
