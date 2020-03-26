826a123ebf2aab0af4c101b6b272791510dc15b6
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-03-26 12:11:06 -0700

    clean config

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,17 +1,6 @@
-export enum PageToDisplay {
-    Home,
-    FaxNumber,
-    Document,
-    Review,
-    Receipt
-}
-
 // export const DEBUG_MANUAL = true;
 export const DEBUG_MANUAL = false;
-export const HOMEPAGE_MANUAL = PageToDisplay.Home;
-
 export const DEBUG = process.env.NODE_ENV === 'production' ? false : DEBUG_MANUAL; //don't use debug in prod
-export const HOMEPAGE = process.env.NODE_ENV === 'production' ? PageToDisplay.Home : HOMEPAGE_MANUAL; //always show homepage in prod
 
 export const BACKEND_ROOT_URL = process.env.NODE_ENV === 'production' ? "https://faxtail.com" : "http://localhost:8001";
 export const GRAPHQL_ENDPOINT = `${BACKEND_ROOT_URL}/graphql`;
