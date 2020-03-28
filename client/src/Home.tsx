88c8f860db819781ebb5cd16311fae7be89eec1b
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-03-28 12:06:29 -0700

    add tooltips to options

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -107,20 +107,39 @@ export default function Home() {
             <Box mx={30}>
                 <Grid component="label" container alignItems="center" justify="center" spacing={1}
                       style={{flexWrap: "nowrap"}}>
-                    <Grid item style={{flex: 2, textAlign: "center"}}>Replace first</Grid>
-                    <Grid item style={{flex: 1, textAlign: "center"}}>
+                    <Grid item xs={5} style={{textAlign: "center"}}>
+                        <Tooltip arrow
+                                 title="Replaces only the first time text appears, and ignores all other matches">
+                            <Typography variant="body1">Replace first</Typography>
+                        </Tooltip>
+                    </Grid>
+                    <Grid item xs={2} style={{textAlign: "center"}}>
                         <Switch checked={isGlobal} onChange={() => setIsGlobal(!isGlobal)}/>
                     </Grid>
-                    <Grid item style={{flex: 2, textAlign: "center"}}>Replace all</Grid>
+                    <Grid item xs={5} style={{textAlign: "center"}}>
+                        <Tooltip arrow
+                                 title="Replaces all occurrences of the text being searched for">
+                            <Typography variant="body1">Replace all</Typography>
+                        </Tooltip>
+                    </Grid>
                 </Grid>
 
                 <Grid component="label" container alignItems="center" justify="center" spacing={1}
                       style={{flexWrap: "nowrap"}}>
-                    <Grid item style={{flex: 2, textAlign: "center"}}>In-place</Grid>
-                    <Grid item style={{flex: 1, textAlign: "center"}}>
+                    <Grid item xs={5} style={{textAlign: "center"}}>
+                        <Tooltip arrow
+                                 title="Modifies 'INPUT_FILE.txt' in-place">
+                            <Typography variant="body1">Modify in-place</Typography>
+                        </Tooltip></Grid>
+                    <Grid item xs={2} style={{textAlign: "center"}}>
                         <Switch checked={isInplace} onChange={() => setIsInplace(!isInplace)}/>
                     </Grid>
-                    <Grid item style={{flex: 2, textAlign: "center"}}>Make new file</Grid>
+                    <Grid item xs={5} style={{textAlign: "center"}}>
+                        <Tooltip arrow
+                                 title="Creates a new file called 'OUTPUT_FILE.txt'. Does not modify 'INPUT_FILE.txt'">
+                            <Typography variant="body1">Make new file</Typography>
+                        </Tooltip>
+                    </Grid>
                 </Grid>
             </Box>
 
