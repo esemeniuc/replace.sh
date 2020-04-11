c09d707009617cb8a0b01ebf3e54d82d0688c907
Eric Semeniuc <3838856+esemeniuc@users.noreply.github.com>
2020-04-11 11:35:41 -0700

    update deps, add sentry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,20 @@ import {PageContainer} from "./PageContainer";
 import {GRAPHQL_ENDPOINT} from "./config";
 import ReactGA from 'react-ga';
 import {CssBaseline} from "@material-ui/core";
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https://615bd73a0a5741debde731bbeacffa25@o371822.ingest.sentry.io/5196842",
+    environment: process.env.NODE_ENV,
+    beforeSend(event, hint) {
+        // Check if it is an exception, and if so, show the report dialog
+        if (event.exception) {
+            Sentry.showReportDialog({eventId: event.event_id});
+        }
+        return event;
+    }
+});
+
 
 const client = new ApolloClient({
     cache: new InMemoryCache(),
